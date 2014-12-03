7135d570d521dc15a37f75a2e426b7ac29167cfb
Mikhail Kolmogorov <fenderglass@gmail.com>
2014-12-02 18:16:30 -0800

    multiple windows

--- run_parallel.py
+++ run_parallel.py
@@ -14,6 +14,8 @@ MSALIGN_DIR = os.environ["MSALIGN_DIR"]
 MSALIGN_CMD = ["java", "-Xmx12G", "-classpath", "jar/*:",
                "edu.ucsd.msalign.align.console.MsAlignPipeline"]
 
+WINDOWS = [500, 230, 110, 60]
+
 def run_instance(proteome_file, spectrum_file, work_dir):
     assert os.path.isdir(work_dir)
     os.mkdir(os.path.join(work_dir, "msinput"))
@@ -87,53 +89,59 @@ def run_parallel(input_genome, input_spectrum, work_dir, num_proc):
     if not os.path.isdir(work_dir):
         os.mkdir(work_dir)
     work_dir = os.path.abspath(work_dir)
+
     input_spectrum = os.path.abspath(input_spectrum)
     input_genome = os.path.abspath(input_genome)
     os.chdir(MSALIGN_DIR)
 
-    print("Creating proteome")
-    prot_file_1, prot_file_2 = make_proteome(input_genome, SLICE_SIZE,
-                                             work_dir)
-    print("Reading spectrum")
-    spectras_text = read_spectrum_file(input_spectrum)
+    for window in WINDOWS:
+        window_dir = os.path.join(work_dir, "genome_run_" + str(window))
+        if not os.path.isdir(window_dir):
+            os.mkdir(window_dir)
 
-    proc_per_half = int(num_proc / 2)
-    spec_splitted = split_n(spectras_text, proc_per_half)
-    threads = []
+        print("Creating proteome")
+        prot_file_1, prot_file_2 = make_proteome(input_genome, window,
+                                                 window_dir)
+        print("Reading spectrum")
+        spectras_text = read_spectrum_file(input_spectrum)
 
-    def run_for_half(inst_pref, inst_prot):
-        for i in range(proc_per_half):
-            inst_name = "{0}_{1}".format(inst_pref, i)
-            inst_workdir = os.path.join(work_dir, inst_name)
-            if os.path.isdir(inst_workdir):
-                shutil.rmtree(inst_workdir)
-            os.mkdir(inst_workdir)
+        proc_per_half = int(num_proc / 2)
+        spec_splitted = split_n(spectras_text, proc_per_half)
+        threads = []
 
-            inst_spec = os.path.join(inst_workdir, "spectra.msalign")
-            write_spectras(spec_splitted[i], inst_spec)
+        def run_for_half(inst_pref, inst_prot):
+            for i in range(proc_per_half):
+                inst_name = "{0}_{1}".format(inst_pref, i)
+                inst_workdir = os.path.join(window_dir, inst_name)
+                if os.path.isdir(inst_workdir):
+                    shutil.rmtree(inst_workdir)
+                os.mkdir(inst_workdir)
 
-            print("Running {0} instance".format(inst_name))
-            thread = Thread(target=run_instance,
-                            args=(inst_prot, inst_spec, inst_workdir))
-            thread.start()
-            threads.append(thread)
+                inst_spec = os.path.join(inst_workdir, "spectra.msalign")
+                write_spectras(spec_splitted[i], inst_spec)
 
-    run_for_half("noshift", prot_file_1)
-    run_for_half("halfshift", prot_file_2)
+                print("Running {0} instance".format(inst_name))
+                thread = Thread(target=run_instance,
+                                args=(inst_prot, inst_spec, inst_workdir))
+                thread.start()
+                threads.append(thread)
 
-    for t in threads:
-        t.join()
+        run_for_half("noshift", prot_file_1)
+        run_for_half("halfshift", prot_file_2)
+
+        for t in threads:
+            t.join()
 
 
 def main():
-    if len(sys.argv) != 6:
+    if len(sys.argv) != 5:
         print("Usage: run_parallel.py genome specrum workdir "
-              "num_proc slice_size",
+              "num_proc",
               file=sys.stderr)
         return 1
 
     run_parallel(sys.argv[1], sys.argv[2], sys.argv[3],
-                 int(sys.argv[4]), int(sys.argv[5]))
+                 int(sys.argv[4]))
     return 0
 
 
